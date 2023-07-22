a6175c28e6fcec7f04ee779a5c04f0f244f4d53d
Mayor04 <mayorwa4@gmail.com>
2023-07-22 16:19:00 +0100

    fix: remove super overlay dependency

--- lib/super_overlay.dart
+++ lib/super_overlay.dart
@@ -1,9 +1,4 @@
 library super_overlay;
 
-export 'src/super_overlay_widget.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// export 'src/super_overlay_widget.dart';
+export 'src/animated_overlay.dart';
